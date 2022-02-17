e93953910bd2f7c4e992edb834fce9ea804deb5a
BasiliskOps <noro.nero@gmail.com>
2022-02-16 22:37:00 -0700

    conditional statement for #2 & 3

--- 8magick.py
+++ 8magick.py
@@ -3,4 +3,7 @@ import random
 def get_answer(answer_number):
     if answer_number == 1:
         return 'It is certain'
-    
+    elif answer_number == 2:
+        return 'It is decidedly so'
+    elif answer_number == 3:
+        return 'Yes'
