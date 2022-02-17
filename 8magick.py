7fa6585be5b5e1c33deff950db0c1d04bc07cbac
BasiliskOps <noro.nero@gmail.com>
2022-02-16 23:02:28 -0700

    comment of future plans for the program

--- 8magick.py
+++ 8magick.py
@@ -20,13 +20,13 @@ def get_answer(answer_number):
     elif answer_number == 9:
         return 'Very doubtful'
 
-# x = random.randint(1, 9)
-# fortune = get_answer(x)
 
 print('What is it that you seek, traveler?')
 desire = input()
 
 print(get_answer(random.randint(1, 9)))
+
+# add a gui to scale the program up from a command line/interactive shell program
 
     
 
